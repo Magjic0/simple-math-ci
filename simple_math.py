bfdad46d038ebd22140e176b3688a5295bd56d3a
Magjic0 <magjico74@gmail.com>
2025-07-01 21:39:33 +0200

    Ajout de SimpleMath.addition et test unitaire

--- simple_math.py
+++ simple_math.py
@@ -4,3 +4,7 @@ class SimpleMath:
     @staticmethod
     def addition(a: float, b: float) -> float:
         return a + b
+
+    @staticmethod
+    def subtraction(a: float, b: float) -> float:
+        return a - b
--- tests/test_simple_math.py
+++
@@ -1,4 +0,0 @@
-from simple_math import SimpleMath
-
-def test_addition():
-    assert SimpleMath.addition(2, 3) == 5
